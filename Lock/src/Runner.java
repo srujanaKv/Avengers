fdb1e5a83e7450ece8ec109e994ef596d36646b1
srujanaKv <srujanakoppadka@gmail.com>
2025-04-10 12:30:20 +0530

    PUSH CODE TO GIT

---
+++ Lock/src/Runner.java
@@ -0,0 +1,10 @@
+import com.sruj.external.Lock;
+import com.sruj.internal.Security;
+
+public class Runner {
+    public static void main(String [] args){
+        System.out.println("Lock is created");
+        Lock lock=new Lock();
+        Security security=new Lock();
+    }
+}
---
+++ Lock/src/com/sruj/external/Lock.java
@@ -0,0 +1,10 @@
+package com.sruj.external;
+
+import com.sruj.internal.Security;
+
+public class Lock extends Security {
+    public Lock(){
+        System.out.println("Lock is running");
+    }
+
+}
