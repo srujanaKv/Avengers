a75d3b23c27d8d485612729055ac26ac8c8efc3b
srujanaKv <srujanakoppadka@gmail.com>
2025-04-28 22:12:07 +0530

    Interfaces

---
+++ Tickets/src/in/sruj/Ticket/TicketRunner.java
@@ -0,0 +1,10 @@
+package in.sruj.Ticket;
+
+public class TicketRunner {
+    public static void main(String []args){
+        Movie movie1=new Movie("mungarumale",111);
+        /*Ticket ticket=new Ticket("ipl",cost:1234.78);
+       Ticket.display();*/
+    }
+
+}
