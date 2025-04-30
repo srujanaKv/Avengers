b4978a6d3011c60be8b51c88889ffb38046f1800
srujanaKv <srujanakoppadka@gmail.com>
2025-04-30 08:50:41 +0530

    Strings

---
+++ Replacevowels/src/Replacevowels.java
@@ -0,0 +1,23 @@
+public class Replacevowels {
+    public static void main(String[] args) {
+        String input = "you are welcome";
+        input = input.toLowerCase();
+        String result = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if ("aeiou".indexOf(ch) != -1) {
+                char nextChar = (char)(ch + 1);
+                while ("aeiou".indexOf(nextChar) != -1) {
+                    nextChar++;
+                }
+                result += nextChar;
+            } else {
+                result += ch;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
