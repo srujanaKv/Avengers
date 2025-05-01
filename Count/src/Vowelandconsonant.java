402455ecbb896ba773c8c49d6dea405bc246f752
srujanaKv <srujanakoppadka@gmail.com>
2025-05-01 22:23:29 +0530

    Strings

---
+++ Count/src/Vowelandconsonant.java
@@ -0,0 +1,16 @@
+public class Vowelandconsonant {
+    public static void main(String[] args) {
+
+        String input = "RCB will win the match";
+        int v_count = 0;
+        int c_count = 0;
+        for (int index = 0; index < input.length(); index++) {
+
+            if (input.charAt(index) == 'A' || input.charAt(i) == 'i' || input.charAt(i) == '0' || input.charAt(int index));
+            v_count++;
+        }
+        else{
+            c_count++;
+        }
+    }
+}
