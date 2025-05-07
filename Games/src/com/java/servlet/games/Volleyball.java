f891ca669d9af53c3a77bee97b4a4c50585ab0b1
srujanaKv <srujanakoppadka@gmail.com>
2025-05-07 23:57:07 +0530

    servlet

--- Games/src/com/java/servlet/games/Volleyball.java
+++ Games/src/com/java/servlet/games/Volleyball.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/send",loadOnStartup = 1)
+@WebServlet(urlPatterns = "/volleyball",loadOnStartup = 1)
 
 public class Volleyball extends GenericServlet {
     public Volleyball(){
