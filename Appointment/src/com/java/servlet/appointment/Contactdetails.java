bd5a0e0731f7a2de6c0c3d6d3db1545d29c3162d
srujanaKv <srujanakoppadka@gmail.com>
2025-05-12 10:40:12 +0530

    Servlet

--- Appointment/src/com/java/servlet/appointment/Contactdetails.java
+++ Appointment/src/com/java/servlet/appointment/Contactdetails.java
@@ -7,44 +7,36 @@ import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/appointment")
-public class Appointment extends GenericServlet {
-    public Appointment(){
-        System.out.println("no args constructor");
+@WebServlet(urlPatterns = "/contact")
+public class Contactdetails extends GenericServlet {
+    public Contactdetails(){
+        System.out.println("no-args const");
     }
-
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("Service is running");
+        System.out.println("service is running");
 
         String text=servletRequest.getParameter("firstname");
         String lastname=servletRequest.getParameter("lastname");
-        String email =servletRequest.getParameter("email");
+        String email=servletRequest.getParameter("email");
         String tel=servletRequest.getParameter("phoneNumber");
-        long phoneNumber=0;
-        if(tel!=null) {
-            phoneNumber = Long.parseLong(tel);
+        long number=0;
+        if(tel!=null){
+            number=Long.parseLong(tel);
         }
-        String number= servletRequest.getParameter("Age");
-        int age=0;
-        if(number!=null){
-            age=Integer.parseInt(number);
-        }
+        String message=servletRequest.getParameter("Message");
         String Address=servletRequest.getParameter("Address");
-        String Gender=servletRequest.getParameter("Gender");
-        String Sickness=servletRequest.getParameter("Sickness");
 
         servletResponse.setContentType("text/html");
         PrintWriter writer=servletResponse.getWriter();
-
-        writer.println("<h1>Thank you for appointing</h2>");
+        writer.println("<h2>Thank you for contacting</h2>");
         writer.println("<h3>firstname</h3>"+text);
         writer.println("<h3>lastname</h3>"+lastname);
         writer.println("<h3>email</h3>"+email);
         writer.println("<h3>phonenumber</h3>"+tel);
-        writer.println("<h3>Age</h3>"+age);
-        writer.println("<h3>Gender</h3>"+Gender);
-        writer.println("<h3>Sickness</h3>"+Sickness);
+        writer.println("<h3>message</h3>"+message);
+        writer.println("<h3>Address</h3>"+Address);
+
 
     }
 }
