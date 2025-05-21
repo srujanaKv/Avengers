7103ceafa5726b4da1c38ce215d3f367a939fc28
srujanaKv <srujanakoppadka@gmail.com>
2025-05-21 11:15:04 +0530

    service

---
+++ Furniture/src/com/java/service/FIRService.java
@@ -0,0 +1,7 @@
+package com.java.service;
+
+import com.java.furniture.dto.FirDto;
+
+public interface FIRService {
+    boolean save(FirDto firDto);
+}
---
+++ Furniture/src/com/java/serviceimpl/FIRImpl.java
@@ -0,0 +1,11 @@
+package com.java.serviceimpl;
+
+import com.java.furniture.dto.FirDto;
+import com.java.service.FIRService;
+
+public class FIRImpl implements FIRService {
+    @Override
+    public boolean save(FirDto firDto) {
+        return false;
+    }
+}
