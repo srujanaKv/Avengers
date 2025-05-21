973ece2c88cfa477cf6717d033d0c322b3344bc4
srujanaKv <srujanakoppadka@gmail.com>
2025-05-21 11:15:32 +0530

    service

---
+++ Furniture/src/com/java/service/FurnitureService.java
@@ -0,0 +1,7 @@
+package com.java.service;
+
+import com.java.furniture.dto.FurnitureDto;
+
+public interface FurnitureService {
+    boolean save(FurnitureDto furnitureDto);
+}
---
+++ Furniture/src/com/java/serviceimpl/FurnitureImpl.java
@@ -0,0 +1,11 @@
+package com.java.serviceimpl;
+
+import com.java.furniture.dto.FurnitureDto;
+import com.java.service.FurnitureService;
+
+public class FurnitureImpl implements FurnitureService {
+    @Override
+    public boolean save(FurnitureDto furnitureDto) {
+        return false;
+    }
+}
