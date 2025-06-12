deaaef100f51b96a0ccb16893e5e8f7d15a10827
srujanaKv <srujanakoppadka@gmail.com>
2025-06-12 21:51:48 +0530

    exception

---
+++ Exceptionn/src/com/java/exception/ShowExceptions.java
@@ -0,0 +1,221 @@
+package com.java.exception;
+
+import jdk.internal.org.xml.sax.SAXException;
+import org.omg.CORBA.portable.RemarshalException;
+
+import javax.management.*;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+import javax.management.modelmbean.XMLParseException;
+import javax.naming.*;
+import javax.print.*;
+import javax.security.auth.DestroyFailedException;
+import javax.security.auth.RefreshFailedException;
+import javax.sound.midi.*;
+import javax.sound.sampled.*;
+import javax.transaction.xa.XAException;
+import javax.xml.crypto.*;
+import javax.xml.crypto.dsig.*;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.parsers.*;
+import javax.xml.soap.*;
+import javax.xml.stream.*;
+import java.awt.datatransfer.MimeTypeParseException;
+import java.awt.print.PrinterException;
+import java.io.*;
+import java.lang.instrument.*;
+import java.lang.invoke.*;
+import java.rmi.*;
+import java.rmi.activation.*;
+import java.rmi.server.*;
+import java.security.*;
+import java.security.acl.*;
+import java.security.cert.*;
+import java.sql.SQLException;
+import java.util.TooManyListenersException;
+import java.util.concurrent.*;
+import java.util.zip.*;
+
+public class ShowExceptions {
+
+    public void getAclNotFoundException() throws AclNotFoundException {
+        throw new AclNotFoundException();
+    }
+
+    public void getActivationException() throws ActivationException {
+        throw new ActivationException();
+    }
+
+    public void getAlreadyBoundException() throws AlreadyBoundException {
+        throw new AlreadyBoundException();
+    }
+
+    public void getBrokenBarrierException() throws BrokenBarrierException {
+        throw new BrokenBarrierException();
+    }
+
+    public void getCertificateException() throws CertificateException {
+        throw new CertificateException();
+    }
+
+    public void getCloneNotSupportedException() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    public void getDataFormatException() throws DataFormatException {
+        throw new DataFormatException();
+    }
+
+    public void getDatatypeConfigurationException() throws DatatypeConfigurationException {
+        throw new DatatypeConfigurationException();
+    }
+
+    public void getDestroyFailedException() throws DestroyFailedException {
+        throw new DestroyFailedException();
+    }
+
+    public void getGeneralSecurityException() throws GeneralSecurityException {
+        throw new GeneralSecurityException();
+    }
+
+    public void getIllegalClassFormatException() throws IllegalClassFormatException {
+        throw new IllegalClassFormatException();
+    }
+
+    public void getInterruptedException() throws InterruptedException {
+        throw new InterruptedException();
+    }
+
+    public void getInvalidMidiDataException() throws InvalidMidiDataException {
+        throw new InvalidMidiDataException();
+    }
+
+    public void getInvalidTargetObjectTypeException() throws InvalidTargetObjectTypeException {
+        throw new InvalidTargetObjectTypeException();
+    }
+
+    public void getIOException() throws IOException {
+        throw new IOException();
+    }
+
+    public void getJMException() throws JMException {
+        throw new JMException();
+    }
+
+    public void getKeySelectorException() throws KeySelectorException {
+        throw new KeySelectorException();
+    }
+
+    public void getLambdaConversionException() throws LambdaConversionException {
+        throw new LambdaConversionException();
+    }
+
+    public void getLastOwnerException() throws LastOwnerException {
+        throw new LastOwnerException();
+    }
+
+    public void getLineUnavailableException() throws LineUnavailableException {
+        throw new LineUnavailableException();
+    }
+
+    public void getMidiUnavailableException() throws MidiUnavailableException {
+        throw new MidiUnavailableException();
+    }
+
+    public void getMimeTypeParseException() throws MimeTypeParseException {
+        throw new MimeTypeParseException();
+    }
+
+    public void getNamingException() throws NamingException {
+        throw new NamingException();
+    }
+
+    public void getNotBoundException() throws NotBoundException {
+        throw new NotBoundException();
+    }
+
+    public void getNotOwnerException() throws NotOwnerException {
+        throw new NotOwnerException();
+    }
+
+    public void getParserConfigurationException() throws ParserConfigurationException {
+        throw new ParserConfigurationException();
+    }
+
+    public void getPrinterException() throws PrinterException {
+        throw new PrinterException();
+    }
+
+    public void getPrintException() throws PrintException {
+        throw new PrintException();
+    }
+
+    public void getReflectiveOperationException() throws ReflectiveOperationException {
+        throw new ReflectiveOperationException();
+    }
+
+    public void getRefreshFailedException() throws RefreshFailedException {
+        throw new RefreshFailedException();
+    }
+
+    public void getRemarshalException() throws RemarshalException {
+        throw new RemarshalException();
+    }
+
+    public void getSAXException() throws SAXException {
+        throw new SAXException();
+    }
+
+    public void getServerNotActiveException() throws ServerNotActiveException {
+        throw new ServerNotActiveException();
+    }
+
+    public void getSOAPException() throws SOAPException {
+        throw new SOAPException();
+    }
+
+    public void getSQLException() throws SQLException {
+        throw new SQLException();
+    }
+
+    public void getTimeoutException() throws TimeoutException {
+        throw new TimeoutException();
+    }
+
+    public void getTooManyListenersException() throws TooManyListenersException {
+        throw new TooManyListenersException();
+    }
+
+
+    public void getTransformException() throws TransformException {
+        throw new TransformException();
+    }
+
+    public void getUnmodifiableClassException() throws UnmodifiableClassException {
+        throw new UnmodifiableClassException();
+    }
+
+    public void getUnsupportedAudioFileException() throws UnsupportedAudioFileException {
+        throw new UnsupportedAudioFileException();
+    }
+
+    public void getURIReferenceException() throws URIReferenceException {
+        throw new URIReferenceException();
+    }
+
+    public void getXAException() throws XAException {
+        throw new XAException();
+    }
+
+    public void getXMLParseException() throws XMLParseException {
+        throw new XMLParseException();
+    }
+
+    public void getXMLSignatureException() throws XMLSignatureException {
+        throw new XMLSignatureException();
+    }
+
+    public void getXMLStreamException() throws XMLStreamException {
+        throw new XMLStreamException();
+    }
+
+}
