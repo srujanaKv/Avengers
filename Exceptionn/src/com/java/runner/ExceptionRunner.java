094bff7932667f99eac875654fbbcc2c4a66bd4c
srujanaKv <srujanakoppadka@gmail.com>
2025-06-12 10:27:25 +0530

    exception

---
+++ Exceptionn/src/com/java/runner/ExceptionRunner.java
@@ -0,0 +1,131 @@
+package com.java.runner;
+
+import com.java.exception.ShowExceptions;
+import jdk.internal.org.xml.sax.SAXException;
+import org.ietf.jgss.GSSException;
+import org.omg.CORBA.portable.ApplicationException;
+import org.omg.CORBA.portable.RemarshalException;
+
+import javax.management.*;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+import javax.management.modelmbean.XMLParseException;
+import javax.naming.NamingException;
+import javax.print.PrintException;
+import javax.script.ScriptException;
+import javax.security.auth.DestroyFailedException;
+import javax.security.auth.RefreshFailedException;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.tree.ExpandVetoException;
+import javax.transaction.xa.XAException;
+import javax.xml.bind.JAXBException;
+import javax.xml.crypto.KeySelectorException;
+import javax.xml.crypto.URIReferenceException;
+import javax.xml.crypto.dsig.TransformException;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.SOAPException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathException;
+import java.awt.*;
+import java.awt.datatransfer.MimeTypeParseException;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.print.PrinterException;
+import java.beans.IntrospectionException;
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.UnmodifiableClassException;
+import java.lang.invoke.LambdaConversionException;
+import java.net.URISyntaxException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.activation.ActivationException;
+import java.rmi.server.ServerNotActiveException;
+import java.security.GeneralSecurityException;
+import java.security.PrivilegedActionException;
+import java.security.acl.AclNotFoundException;
+import java.security.acl.LastOwnerException;
+import java.security.acl.NotOwnerException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.TooManyListenersException;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.InvalidPreferencesFormatException;
+import java.util.zip.DataFormatException;
+
+public class ExceptionRunner {
+    public static void main(String[] args) throws BadAttributeValueExpException, AWTException, AclNotFoundException, ActivationException, AlreadyBoundException, GeneralSecurityException, BadBinaryOpValueExpException, BadLocationException, BadStringOperationException, BrokenBarrierException, CloneNotSupportedException, DataFormatException, ExecutionException, BackingStoreException, FontFormatException, IllegalClassFormatException, InterruptedException, InvalidApplicationException, InvalidMidiDataException, InvalidPreferencesFormatException, IOException, JMException, JAXBException, KeySelectorException, LambdaConversionException, LastOwnerException, LineUnavailableException, MidiUnavailableException, NamingException, NotOwnerException, ParseException, ParserConfigurationException, PrintException, PrivilegedActionException, PropertyVetoException, ReflectiveOperationException, ServerNotActiveException, SOAPException, TimeoutException, TransformException, TransformerException, UnmodifiableClassException, UnsupportedAudioFileException, UnsupportedCallbackException, URIReferenceException, URISyntaxException, XMLSignatureException, XMLStreamException, XPathException, NotBoundException, XMLParseException, XAException, UnsupportedLookAndFeelException, UnsupportedFlavorException, TooManyListenersException, SQLException, ScriptException, SAXException, RemarshalException, RefreshFailedException, PrinterException, NoninvertibleTransformException, MimeTypeParseException, InvalidTargetObjectTypeException, IntrospectionException, GSSException, DatatypeConfigurationException, DestroyFailedException, ExpandVetoException, ApplicationException {
+        ShowExceptions showExceptions=new ShowExceptions();
+        showExceptions.getAclNotFoundException();
+        showExceptions.getActivationException();
+        showExceptions.getAlreadyBoundException();
+
+        showExceptions.getBrokenBarrierException();
+        showExceptions.getCertificateException();
+        showExceptions.getCloneNotSupportedException();
+        showExceptions.getDataFormatException();
+        showExceptions.getDatatypeConfigurationException();
+        showExceptions.getDestroyFailedException();
+
+        showExceptions.getGeneralSecurityException();
+
+        showExceptions.getIllegalClassFormatException();
+        showExceptions.getInterruptedException();
+
+        showExceptions.getInvalidMidiDataException();
+
+        showExceptions.getInvalidTargetObjectTypeException();
+        showExceptions.getIOException();
+
+        showExceptions.getJMException();
+        showExceptions.getKeySelectorException();
+        showExceptions.getLambdaConversionException();
+        showExceptions.getLastOwnerException();
+        showExceptions.getLineUnavailableException();
+
+        showExceptions.getMidiUnavailableException();
+        showExceptions.getMimeTypeParseException();
+        showExceptions.getNamingException();
+        showExceptions.getNotBoundException();
+        showExceptions.getNotOwnerException();
+
+        showExceptions.getParserConfigurationException();
+        showExceptions.getPrinterException();
+        showExceptions.getPrintException();
+
+        showExceptions.getReflectiveOperationException();
+        showExceptions.getRefreshFailedException();
+        showExceptions.getRemarshalException();
+        showExceptions.getSAXException();
+
+        showExceptions.getServerNotActiveException();
+        showExceptions.getSOAPException();
+        showExceptions.getSQLException();
+        showExceptions.getTimeoutException();
+        showExceptions.getTooManyListenersException();
+
+        showExceptions.getTransformException();
+        showExceptions.getUnmodifiableClassException();
+        showExceptions.getUnsupportedAudioFileException();
+
+        showExceptions.getURIReferenceException();
+
+        showExceptions.getXAException();
+        showExceptions.getXMLParseException();
+        showExceptions.getXMLSignatureException();
+        showExceptions.getXMLStreamException();
+
+    }
+}
