64dddae0b19b0db3a1b4e2736b80d52aa57053f7
srujanaKv <srujanakoppadka@gmail.com>
2025-06-17 22:55:04 +0530

    jdbc

---
+++ EarthWorm/src/com/xworkz/earthworm/constant/EWProperties.java
@@ -0,0 +1,27 @@
+package com.xworkz.earthworm.constant;
+
+public enum EWProperties {
+
+    URL("jdbc:mysql://localhost:3306/earthWorm"),
+    USER_NAME("root"),
+    SECRET("Srujana#2001"),
+    VERSION_8;
+
+    private String prop;
+
+    private  EWProperties(String prop)
+    {
+        this.prop=prop;
+    }
+    private EWProperties()
+    {
+
+    }
+
+    public String getProp() {
+        return prop;
+    }
+}
+
+
+
