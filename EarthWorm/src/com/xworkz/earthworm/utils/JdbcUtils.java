9f5c34482ac8fec33bf390e29030a835d315b533
srujanaKv <srujanakoppadka@gmail.com>
2025-06-19 13:31:54 +0530

    jdbc

---
+++ EarthWorm/src/com/xworkz/earthworm/utils/JdbcUtils.java
@@ -0,0 +1,15 @@
+package com.xworkz.earthworm.utils;
+
+
+public class JdbcUtils {
+
+    private JdbcUtils(){
+
+    }
+
+    public static final String jdbcUrl="jdbc:mysql://localhost:3306/earthworm";
+    public static final String jdbcUserName ="root";
+    public static final String jdbcPassword="Srujana#2001";
+    public static final String jdbcDriver ="com.mysql.cj.jdbc.Driver";
+
+}
