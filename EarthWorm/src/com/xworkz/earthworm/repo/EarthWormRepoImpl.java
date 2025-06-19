954de9c07fdb2d5f78040415ea60e8be6fb839b4
srujanaKv <srujanakoppadka@gmail.com>
2025-06-19 13:31:29 +0530

    jdbc

--- EarthWorm/src/com/xworkz/earthworm/repo/EarthWormRepoImpl.java
+++ EarthWorm/src/com/xworkz/earthworm/repo/EarthWormRepoImpl.java
@@ -1,7 +1,8 @@
 package com.xworkz.earthworm.repo;
 
-import com.xworkz.earthworm.constant.EWProperties;
+import com.xworkz.earthworm.utils.JdbcUtils;
 import com.xworkz.earthworm.dto.EarthWormDto;
+import com.xworkz.earthworm.utils.JdbcUtils;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -17,7 +18,78 @@ public class EarthWormRepoImpl implements EarthWormRepo {
             String password = "Srujana#2001";
 
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
+                Class.forName(JdbcUtils.jdbcDriver);
+                //get connection
+                Connection connection = DriverManager.getConnection(JdbcUtils.jdbcUrl, JdbcUtils.jdbcUserName, JdbcUtils.jdbcPassword);
+                String query = "insert into earthworm values(Scientific_Name,Body_Shape,Color,n_female,n_lifespan,Food) values(?,?,?,?,?,?)";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1, earthWormDto.getScientificName());
+                statement.setString(2, earthWormDto.getBodyShape());
+                statement.setString(3, earthWormDto.getColor());
+                statement.setBoolean(4, earthWormDto.getN_female());
+                statement.setInt(5, earthWormDto.getN_lifespan());
+                statement.setString(6, earthWormDto.getFood());
+                int result = statement.executeUpdate();
+                if (result > 0) {
+                    return true;
+                }
+            } catch (ClassNotFoundException | SQLException exception) {
+                exception.printStackTrace();
+            }
+
+            return false;
+        }
+
+        @Override
+        public EarthWormDto[] findAll() {
+            int index = 0;
+            int arraySize = 1;
+            EarthWormDto[] earthWormDtos = null;
+
+            try {
+                Class.forName(JdbcUtils.jdbcDriver);
+                //get connection
+                Connection connection = DriverManager.getConnection(JdbcUtils.jdbcUrl, JdbcUtils.jdbcUserName, JdbcUtils.jdbcPassword);
+                String fetchAll = "select * from earthworm";
+                PreparedStatement statement = connection.prepareStatement(fetchAll);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    earthWormDtos = new EarthWormDto[][arraySize];
+                    System.out.println(resultSet.getInt(1));
+                    System.out.println(resultSet.getString(2));
+                    System.out.println(resultSet.getString(3));
+                    System.out.println(resultSet.getString(4));
+                    System.out.println(resultSet.getBoolean(5));
+                    System.out.println(resultSet.getInt(6));
+                    System.out.println(resultSet.getString(7));
+                    System.out.println("==========================================");
+                    EarthWormDto earthWormDto1 = new EarthWormDto();
+                    earthWormDto1.setEarthId(resultSet.getInt(1));
+                    earthWormDto1.setScientificName(resultSet.getString(2));
+                    earthWormDto1.setBodyShape(resultSet.getString(3));
+                    earthWormDto1.setColor(resultSet.getString(4));
+                    earthWormDto1.setN_female(resultSet.getBoolean(5));
+                    earthWormDto1.setN_lifespan(resultSet.getInt(6));
+                    earthWormDto1.setFood(resultSet.getString(7));
+
+                    earthWormDtos[index] = earthWormDto;
+                    arraySize++;
+                    index++;
+
+
+                }
+
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+
+            return earthWormDtos;
+        }
+    }
+}
+
+
+              /*  Class.forName("com.mysql.cj.jdbc.Driver");
                 //Step 2 : Create the Connection
                 Connection connection = DriverManager.getConnection(url, username, password);
 
@@ -33,9 +105,9 @@ public class EarthWormRepoImpl implements EarthWormRepo {
             return true;
         }
         return false;
-    }
+    }  */
 
-    @Override
+   /* @Override
     public Optional<EarthWormDto> findById(int id) {
         System.out.println("running findById in EarthWormRepoImpl...");
 
@@ -45,19 +117,20 @@ public class EarthWormRepoImpl implements EarthWormRepo {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(EWProperties.URL.getProp(), EWProperties.USER_NAME.getProp(), EWProperties.SECRET.getProp());
-            String findByIdSQL = "select * from earthworm worm where worm.id=" + id + " ";
+            String findByIdSQL = "select * from earthworm EarthWorm where EarthWorm.id=" + id + " ";
             System.out.println("findById :" + findByIdSQL);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(findByIdSQL);
             System.out.println("resultSet :" + resultSet);
             while (resultSet.next()) {
                 int pk = resultSet.getInt("id");
-                String scientificName = resultSet.getString("ScientificName");
-                String bodyShape = resultSet.getString("BodyShape");
+                String scientificName = resultSet.getString("Scientific_Name");
+                String bodyShape = resultSet.getString("Body_Shape");
                 String color = resultSet.getString("Color");
                 Boolean n_female = resultSet.getBoolean("n_female");
                 int n_lifespan = resultSet.getInt("n_lifespan");
                 String food = resultSet.getString("Food");
+               // Timestamp createdTime=resultSet.getTimestamp("createdTime");
 
                 EarthWormDto earthWormDto = new EarthWormDto(scientificName, bodyShape, color, n_female, n_lifespan, food);
                 return Optional.of(earthWormDto);
@@ -68,5 +141,5 @@ public class EarthWormRepoImpl implements EarthWormRepo {
         }
         return Optional.empty();
     }
-}
+} */
 
