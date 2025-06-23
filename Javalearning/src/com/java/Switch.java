af99fd1171d501988dd28024c8e20650529c70da
srujanaKv <srujanakoppadka@gmail.com>
2025-06-23 21:40:27 +0530

    ..

---
+++ Javalearning/src/com/java/Switch.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Switch {
+    public static void main(String[]args){
+
+        int day=3;
+
+        switch (day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("wed-sun");
+        }
+    }
+}
