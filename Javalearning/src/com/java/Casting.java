14f718b6e7c7782acc7d8d49cf126f0fbc7a52ce
srujanaKv <srujanakoppadka@gmail.com>
2025-06-23 21:39:30 +0530

    ..

---
+++ Javalearning/src/com/java/Casting.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Casting {
+    public static void main(String[] args) {
+
+        double price = 10.8;
+        double finalprice = price + 10;
+
+        System.out.println(finalprice);
+
+        int p= 30;
+        int fp = p + (int) 40.6;
+
+        System.out.println(fp);
+    }
+}
+
+//implicit casting and explicit casting=2 types
+// casting is nothing but converting one type to another
