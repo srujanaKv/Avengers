5acfce68c4f58747e4aad70b7097e73c87b1b8d9
srujanaKv <srujanakoppadka@gmail.com>
2025-06-25 14:45:35 +0530

    ..

---
+++ Javalearning/src/com/java/Method.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Method {
+
+    public static void printName(String Name){
+        System.out.println(Name);
+    }
+    public static void PrintSum(int a,int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+
+   /* public static void main(String[] args) {
+        printName("Srujana");
+        printName("Agasthya");  */
+   public static void main(String[] args) {
+       PrintSum(100,87);
+   }
+    }
+
