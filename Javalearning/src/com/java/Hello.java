67fcdd1a0c32c2e3a533bd8a1b6e5fa56a4c5848
srujanaKv <srujanakoppadka@gmail.com>
2025-06-25 14:44:44 +0530

    ..

---
+++ Javalearning/src/com/java/Hello.java
@@ -0,0 +1,51 @@
+package com.java;
+
+import java.util.Locale;
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        //  String name="Srujana";
+        //   int age=23;
+
+        //   System.out.println(name.length());
+        //   System.out.println(name.charAt(3));
+
+        //   String name="Agasthya and Srujana";
+        //   String name2=name.replace('s','a');
+        // String name2=name.toUpperCase(Locale.ROOT);
+
+        //  System.out.println(name2);
+
+        //  System.out.println(name.substring(0,14));
+
+        //Arrayss
+
+       /*  int kannada=90;
+         int social=88;
+        int maths=80;
+
+        int[] marks = new int[3];
+
+        marks[0] = 90;
+        marks[1] = 98;
+        marks[2] = 78;
+
+        System.out.println( marks[0]);
+ /*sorting */      // Arrays.sort(marks);
+      //  System.out.println(marks[0]);
+
+        //dimensions in array(1D,2D)
+
+        int [] score={99,67,89};
+
+        //2D
+
+        int [] [] scores={{98,68,90},{48,90,64}};
+
+        System.out.println(scores[1][2]);
+    }
+
+
+}
+
