d26c6de9fec0316b907a67f92020d8d8b913173b
srujanaKv <srujanakoppadka@gmail.com>
2025-06-25 14:45:12 +0530

    ..

---
+++ Javalearning/src/com/java/Input.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String [] args){
+
+    /*    Scanner sc=new Scanner(System.in);
+        System.out.println("input is:"+"");
+        String name=sc.next();
+        System.out.println(name);   */
+
+     /*  Scanner sc=new Scanner(System.in);
+        System.out.println("enter age");
+        int age=sc.nextLine();
+        System.out.println(age);
+*/
+
+
+    }
+}
