28858c32fc9730acf3f444dec4fd39cb475bec7c
srujanaKv <srujanakoppadka@gmail.com>
2025-06-26 23:58:29 +0530

    ..

---
+++ Javalearning/src/com/java/Variables.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Variables {
+    public static void main(String args[]){
+        int a=40;
+        int b=20;
+      //  int sum=a+b;
+       // System.out.println(sum);
+        int ans=a*b/a+b;
+        System.out.println(ans);
+    }
+}
+ 
