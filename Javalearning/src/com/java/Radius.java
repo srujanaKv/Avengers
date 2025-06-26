c97d72022c739f1713049237a2da9177a93a0dd1
srujanaKv <srujanakoppadka@gmail.com>
2025-06-26 23:56:35 +0530

    ..

---
+++ Javalearning/src/com/java/Radius.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Radius {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int radius=sc.nextInt();
+        int area=sc.nextInt();
+        int sum=radius*area;
+        System.out.println(sum);
+
+//it is wrongg
+    }
+}
