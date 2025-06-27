f7a6fcea3832b73af4823e2f073814fbeca435a6
srujanaKv <srujanakoppadka@gmail.com>
2025-06-27 22:13:04 +0530

    push

---
+++ Javalearning/src/com/java/Operators.java
@@ -0,0 +1,21 @@
+package com.java;
+
+public class Operators {
+    public static void main(String [] args){
+
+        int a=10;
+        int b=70;
+        int sum=a+b;
+
+        System.out.println(sum);
+
+        double price=10;
+        double price2=30;
+         double sum1=price2+price;
+        System.out.println(sum1);
+
+        int numb=1;
+        numb++;
+        System.out.println(numb);
+    }
+}
