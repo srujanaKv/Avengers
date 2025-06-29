55cc04680dc8cefca93bce5b2b0e01b89f19e7d3
srujanaKv <srujanakoppadka@gmail.com>
2025-06-29 21:49:08 +0530

    push

---
+++ Javalearning/src/com/java/Loop.java
@@ -0,0 +1,29 @@
+/* package com.java;
+
+public class Loop {
+    public static void main(String args[]){
+
+        //for loop
+        for(int i=1;i>=100;i++){
+            System.out.println(i);
+        }
+        //while loop
+
+        int j=100;
+        while(int j>=1){
+            System.out.println(j);
+            j=j-1;
+        }
+      //do while
+      int k=100;
+        do{
+            System.out.println(k);
+            k=k-1;
+        }
+        while(k>=1){
+            
+        }
+
+    }
+}
+  */
