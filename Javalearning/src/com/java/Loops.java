cb3535c18adf820b408b9ea06e7da5c36eb5b330
srujanaKv <srujanakoppadka@gmail.com>
2025-07-01 17:18:48 +0530

    push

---
+++ Javalearning/src/com/java/Loops.java
@@ -0,0 +1,28 @@
+package com.java;
+
+public class Loops {
+    public static void main(String args[]) {
+
+        //for loop example
+
+          for(int i=0;i<10;i++){
+           System.out.println(i);
+          }
+
+        // while loop example
+
+       int i = 0;
+        while (i < 10) {
+            i = i + 1;
+            System.out.println(i);
+        }
+
+        //Do while loop
+
+        int sum = 0;
+        do {
+            System.out.println(sum);
+            sum=sum+1;
+        } while (sum < 5) ;
+        }
+    }
