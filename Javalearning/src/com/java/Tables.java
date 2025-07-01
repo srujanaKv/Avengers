f338fb99dfbb6bf6853096903a0139b27a9d1414
srujanaKv <srujanakoppadka@gmail.com>
2025-07-01 17:39:56 +0530

    push

---
+++ Javalearning/src/com/java/Tables.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Tables {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=1;i<11;i++){
+            System.out.println(i*n);
+        }
+    }
+}
