91bf70e79f14536bf1b661e2fc3482017723998e
srujanaKv <srujanakoppadka@gmail.com>
2025-07-02 21:29:52 +0530

    push

--- Javalearning/src/com/java/Patterns.java
+++ Javalearning/src/com/java/Patterns.java
@@ -4,7 +4,9 @@ public class Patterns {
 
     public static void main(String args[]) {
 
-        int n = 4;
+        //Hollow Rectangle
+
+     /*   int n = 4;
         int m = 5;
 
         for (int i = 1; i <= n; i++) {
@@ -13,11 +15,24 @@ public class Patterns {
                 if (i == 1 || j == 1 || i == n || j == m) {
 
                     System.out.print("*");
-                } else{
+                } else {
                     System.out.print(" ");
                 }
-                System.out.println();
             }
+                System.out.println();
+            }     */
+
+      //half pyramid pattern
+
+        int n=4;
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
         }
     }
-}
