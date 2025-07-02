7521ee21ef5c5811a893ee181b8c8cbd0d500795
srujanaKv <srujanakoppadka@gmail.com>
2025-07-02 21:39:32 +0530

    push

--- Javalearning/src/com/java/Patterns.java
+++ Javalearning/src/com/java/Patterns.java
@@ -37,12 +37,25 @@ public class Patterns {
 
         //inverted pyramid
 
-        int n=4;
+     /*   int n=4;
 
         for(int i=n;i>=1;i--){
 
             for(int j=1;j<=i;j++){
                 System.out.print("*");
+            }
+            System.out.println(" ");
+        }   */
+
+        // number pattern
+
+        int n=5;
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+
             }
             System.out.println(" ");
         }
