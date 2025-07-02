c1dd4d97c55799a3803b4127cdc4e4c02f2d1716
srujanaKv <srujanakoppadka@gmail.com>
2025-07-02 21:34:37 +0530

    push

--- Javalearning/src/com/java/Patterns.java
+++ Javalearning/src/com/java/Patterns.java
@@ -24,7 +24,7 @@ public class Patterns {
 
       //half pyramid pattern
 
-        int n=4;
+     /*   int n=4;
 
         for(int i=1;i<=n;i++){
 
@@ -33,6 +33,18 @@ public class Patterns {
                 System.out.print("*");
             }
             System.out.println("");
+        }  */
+
+        //inverted pyramid
+
+        int n=4;
+
+        for(int i=n;i>=1;i--){
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
         }
     }
