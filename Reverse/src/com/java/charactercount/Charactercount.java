aec30bb234874d1a68509b5f74f09a86c10347c7
srujanaKv <srujanakoppadka@gmail.com>
2025-07-05 19:18:44 +0530

    push

---
+++ Reverse/src/com/java/charactercount/Charactercount.java
@@ -0,0 +1,9 @@
+package com.java.charactercount;
+
+public class Charactercount{
+    public static void main(String[] args) {
+        String sentence = "Time is money";
+        int count = sentence.length();
+        System.out.println("Number of characters: " +count);
+}
+}
