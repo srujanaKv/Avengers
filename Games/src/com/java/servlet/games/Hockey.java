b786aa0d90cc7cfa8c85437a2c0043bef9ae5708
srujanaKv <srujanakoppadka@gmail.com>
2025-07-12 22:14:48 +0530

    push

--- Games/src/com/java/servlet/games/Hockey.java
+++ Games/src/com/java/servlet/games/Hockey.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/send",loadOnStartup = 1)
+@WebServlet(urlPatterns = "/hockey",loadOnStartup = 1)
 public class Hockey extends GenericServlet {
 
     public Hockey(){
