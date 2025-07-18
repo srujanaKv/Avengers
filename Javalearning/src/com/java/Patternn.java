79d0420ff5e23d35d0401a29949ec0e41c6c857f
srujanaKv <srujanakoppadka@gmail.com>
2025-07-18 13:44:50 +0530

    push

---
+++ Javalearning/src/com/java/Patternn.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class Patternn {
+    public static void main(String args[]) {
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+
+
+                System.out.println("*");
+
+                System.out.println(" ");
+            }
+        }
+    }
+}
